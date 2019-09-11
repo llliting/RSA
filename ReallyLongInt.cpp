70104e35e35e1e8406d302ca6af61e9d2e8c764e
llliting <hllliting@gmail.com>
2019-09-11 16:01:59 -0400

    create files

---
+++ ReallyLongInt.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <vector>
+#include <string.h>
+
+
+using namespace std;
+
+
+
+class ReallyLongInt{
+    private:
+        vector<bool> *digits;
+        unsigned int size;
+        bool isNeg;
+
+
+    public:
+    ReallyLongInt(){
+
+    }
+
+    ReallyLongInt(long long num){
+
+    }
+
+    ReallyLongInt(const string& numStr){
+
+    }
+
+    ReallyLongInt(const ReallyLongInt& other){
+
+    }
+
+    string toString() const{
+
+    }
+
+    string toStringBinary() const{
+        
+    }
+
+    ~ReallyLongInt(){
+
+    }
+
+};
+
