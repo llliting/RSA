e073f83c14d1b00872e1469cec7af7a82275eb70
llliting <lhuang1@fandm.edu>
2019-09-14 14:59:55 -0400

    finished to string

--- ReallyLongInt.cpp
+++ ReallyLongInt.cpp
@@ -18,21 +18,26 @@ ReallyLongInt::~ReallyLongInt(){
 }
 
 string ReallyLongInt::toString() const{
-    
+    long long num = 0;
+    for(size_t i = 0; i < size; i++){
+        num += ( (*digits)[i] == 1 ? pow(2, i):0);
+    }
+    return (isNeg ? ("-" + to_string(num)) : to_string(num));
 }
 
-string ReallyLongInt::toBinaryString() const{
-    
+string ReallyLongInt::toStringBinary() const{
+    string str;
+    for(size_t i = 0; i < size; ++i)
+        str += ((*digits)[i] == 1 ? "1" : "0");        
+    return str;
 }
-
-
-
-
 
 
 int main(){
     ReallyLongInt a;
     cout << a.size << " " << (*(a.digits))[0] << " " << a.isNeg << endl;
+    cout << a.toStringBinary() << endl;
+    cout << a.toString() << endl;
     return 0;
 }
 
