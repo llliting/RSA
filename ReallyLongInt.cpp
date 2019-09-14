bd58cef9c188b4da3bf356a138ae2856f06d6da5
llliting <lhuang1@fandm.edu>
2019-09-14 15:20:02 -0400

    absgreater

--- ReallyLongInt.cpp
+++ ReallyLongInt.cpp
@@ -13,13 +13,62 @@ ReallyLongInt::ReallyLongInt(){
     isNeg = false;
 }
 
+ReallyLongInt::ReallyLongInt(long long num){
+    //update isNeg
+    if(num >= 0)
+        isNeg = false;
+    else{
+        isNeg = true;
+        num *= -1;
+    }
+    //initialize and update size
+    size = 16;
+    long long max = 1 << size;
+    while(num >= max){
+        max = max << size;
+        size = size * 2;
+    }
+    //fill the digits
+    digits = new vector<bool> (size, false);
+    unsigned int index = 0;
+    while(num > 0){
+        if(num%2 == 1)
+            (*digits)[index] = 1;
+        num /= 2;
+        index ++;
+    }
+}
+
 ReallyLongInt::~ReallyLongInt(){
     delete digits;
 }
 
+
+bool ReallyLongInt::equal(const ReallyLongInt& other) const{
+
+}
+
+bool ReallyLongInt::absGreater(const ReallyLongInt& other) const{
+    if(other.size > size)
+        return 0;
+    else if(other.size < size)
+        return 1;
+    else{
+        for(unsigned int i = size - 1; i >= 0; i --){
+            if(((*digits)[i] == 1) && ((*(other.digits))[i] == 0))            
+                return 1;
+            else if(((*digits)[i] == 0) && ((*(other.digits))[i] == 1))
+                return 0;
+        }
+    }
+    return 0;
+}
+
+
+
 string ReallyLongInt::toString() const{
     long long num = 0;
-    for(size_t i = 0; i < size; i++){
+    for(unsigned int i = 0; i < size; i++){
         num += ( (*digits)[i] == 1 ? pow(2, i):0);
     }
     return (isNeg ? ("-" + to_string(num)) : to_string(num));
@@ -27,15 +76,22 @@ string ReallyLongInt::toString() const{
 
 string ReallyLongInt::toStringBinary() const{
     string str;
-    for(size_t i = 0; i < size; ++i)
+    for(unsigned int i = 0; i < size; ++i)
         str += ((*digits)[i] == 1 ? "1" : "0");        
     return str;
 }
 
 
 int main(){
-    ReallyLongInt a;
+    long long x, y;
+    cout << "type in a number : ";
+    cin >> x;
+    ReallyLongInt a(x);
+    cout << "type in a number : ";
+    cin >> y;
+    ReallyLongInt b(y);
     cout << a.size << " " << (*(a.digits))[0] << " " << a.isNeg << endl;
+    cout << "absgreater" << a.absGreater(b)<< endl;
     cout << a.toStringBinary() << endl;
     cout << a.toString() << endl;
     return 0;
