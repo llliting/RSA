0ce71fbdd79d09bff1df1317664f3d093d06514c
llliting <lhuang1@fandm.edu>
2019-09-14 14:34:43 -0400

    setup

---
+++ ReallyLongInt.cpp
@@ -0,0 +1,37 @@
+
+#ifndef REALLYLONGINT_H
+#define REALLYLONGINT_H
+
+#include <iostream>
+#include <vector>
+#include <bitset>
+#include <string.h>
+
+using namespace std;
+
+
+class ReallyLongInt
+{
+    public:
+   //private:
+        vector<bool>* digits;
+        unsigned int size;
+        bool isNeg;
+        bool absGreater(const ReallyLongInt& other) const;
+        void init(long long num);
+        
+
+    //public:
+        ReallyLongInt();
+        ReallyLongInt(long long num);
+        ReallyLongInt(const string& numStr);
+        ReallyLongInt(const ReallyLongInt& other);
+
+        string toString() const;
+        string toStringBinary() const;
+        bool equal(const ReallyLongInt& other) const;
+        bool greater(const ReallyLongInt& other) const;
+        ~ReallyLongInt();
+};
+
+#endif 
