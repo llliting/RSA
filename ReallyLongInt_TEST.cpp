20b6e04616a98617749981dfd0d70dffeab93c61
llliting <hllliting@gmail.com>
2019-09-16 08:57:18 -0400

    fresh

--- ReallyLongInt_TEST.cpp
+++ ReallyLongInt_TEST.cpp
@@ -2,4 +2,72 @@
 #include "catch.hpp"
 #include "ReallyLongInt.hpp"
 
+TEST_CASE("TESTING ReallyLongInt Class")
+{
+  ReallyLongInt *test_ReallyLongInt;
+
+  SECTION("Default Constructor")
+    {
+     
+      test_ReallyLongInt = new ReallyLongInt();
+
+      INFO("Using default constructor") //Displayed if fails
+      CAPTURE(test_ReallyLongInt->size); //Display this if fails
+    
+      //IF TEST FAIL -- STOP ALL TESTING
+      REQUIRE(test_ReallyLongInt->size == 16); // Check if true
+      REQUIRE_FALSE(test_ReallyLongInt->size != 16); //Check if false
+
+      REQUIRE(test_ReallyLongInt->isNeg == false);
+      REQUIRE_FALSE(test_ReallyLongInt->size != true); //Check if false
+
+      REQUIRE(!(*(test_ReallyLongInt->digits)).empty());
+      REQUIRE_FALSE((*(test_ReallyLongInt->digits)).empty()); //Check if false
+
+      REQUIRE((*(test_ReallyLongInt->digits)).size() == 16);
+      REQUIRE_FALSE((*(test_ReallyLongInt->digits)).size()!=16); //Check if false
+
+      delete test_ReallyLongInt;
+    }
+
+  SECTION("Long Long Constructor")
+    {
+     
+      test_ReallyLongInt = new ReallyLongInt(-9223372036854775808);
+
+      INFO("Using default constructor") //Displayed if fails
+      CAPTURE(test_ReallyLongInt->size); //Display this if fails
+    
+      //IF TEST FAIL -- STOP ALL TESTING
+      REQUIRE(test_ReallyLongInt->size ==64); // Check if true
+      REQUIRE_FALSE(test_ReallyLongInt->size != 64); //Check if false
+
+      REQUIRE(test_ReallyLongInt->isNeg == true);
+      REQUIRE_FALSE(test_ReallyLongInt->size != false); //Check if false
+
+      REQUIRE(!(*(test_ReallyLongInt->digits)).empty());
+      REQUIRE_FALSE((*(test_ReallyLongInt->digits)).empty()); //Check if false
+
+      REQUIRE((*(test_ReallyLongInt->digits)).size() == 64);
+      REQUIRE_FALSE((*(test_ReallyLongInt->digits)).size()!=64); //Check if false
+
+      delete test_ReallyLongInt;
+    }
+
+  SECTION("Max function")
+    {
+      test_vector = new Vector(10, 5.5);      
+      REQUIRE(test_vector->max() == 5.5);
+    }
+}
+
+TEST_CASE("Testing Nothing")
+{
+  SECTION("Nothing One")
+    {
+      REQUIRE(true);
+    }
+
+
+}
 
