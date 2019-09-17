2cb094ab5e4096131c739c06d60403d1b869a7ec
llliting <lhuang1@fandm.edu>
2019-09-16 20:31:45 -0400

    finished tesrt

--- ReallyLongInt_TEST.cpp
+++ ReallyLongInt_TEST.cpp
@@ -1,6 +1,7 @@
 #define CATCH_CONFIG_MAIN //This will make our main for us :)
 #include "catch.hpp"
 #include "ReallyLongInt.hpp"
+#include <string>
 
 TEST_CASE("TESTING ReallyLongInt Class")
 {
@@ -12,9 +13,7 @@ TEST_CASE("TESTING ReallyLongInt Class")
       test_ReallyLongInt = new ReallyLongInt();
 
       INFO("Using default constructor") //Displayed if fails
-      CAPTURE(test_ReallyLongInt->size); //Display this if fails
-    
-      //IF TEST FAIL -- STOP ALL TESTING
+
       REQUIRE(test_ReallyLongInt->size == 16); // Check if true
       REQUIRE(test_ReallyLongInt->isNeg == false);
       REQUIRE(!(*(test_ReallyLongInt->digits)).empty());
@@ -22,68 +21,133 @@ TEST_CASE("TESTING ReallyLongInt Class")
       delete test_ReallyLongInt;
     }
 
+
+
+
   SECTION("Long Long Constructor")
     {
      
-      test_ReallyLongInt = new ReallyLongInt(-1152921504606846976);
+      test_ReallyLongInt = new ReallyLongInt(-4294967296);
 
-      INFO("Using default constructor") //Displayed if fails
-      CAPTURE(test_ReallyLongInt->size); //Display this if fails
-    
-      //IF TEST FAIL -- STOP ALL TESTING
+      INFO("Using long long constructor") //Displayed if fails
+
       REQUIRE(test_ReallyLongInt->size ==64); // Check if true
-      REQUIRE_FALSE(test_ReallyLongInt->size != 64); //Check if false
-
       REQUIRE(test_ReallyLongInt->isNeg == true);
-      REQUIRE_FALSE(test_ReallyLongInt->size != false); //Check if false
-
       REQUIRE(!(*(test_ReallyLongInt->digits)).empty());
-      REQUIRE_FALSE((*(test_ReallyLongInt->digits)).empty()); //Check if false
-
       REQUIRE((*(test_ReallyLongInt->digits)).size() == 64);
-      REQUIRE_FALSE((*(test_ReallyLongInt->digits)).size()!=64); //Check if false
+      REQUIRE((*(test_ReallyLongInt->digits))[32] == 1);
 
       delete test_ReallyLongInt;
     }
 
-    SECTION("Default Constructor")
+
+
+
+   SECTION("String Constructor")
     {
      
-      test_ReallyLongInt = new ReallyLongInt("2305843009213693952");
+      test_ReallyLongInt = new ReallyLongInt("-4294967296");
 
-      INFO("Using default constructor") //Displayed if fails
-      CAPTURE(test_ReallyLongInt->size); //Display this if fails
-    
-      //IF TEST FAIL -- STOP ALL TESTING
-      REQUIRE(test_ReallyLongInt->size == 64); // Check if true
-      REQUIRE_FALSE(test_ReallyLongInt->size != 64); //Check if false
-
-      REQUIRE(test_ReallyLongInt->isNeg == false);
-      REQUIRE_FALSE(test_ReallyLongInt->size != false); //Check if false
-
+      INFO("Using string constructor") //Displayed if fails
+      REQUIRE(test_ReallyLongInt->size ==64); // Check if true
+      REQUIRE(test_ReallyLongInt->isNeg == true);
       REQUIRE(!(*(test_ReallyLongInt->digits)).empty());
-      REQUIRE_FALSE((*(test_ReallyLongInt->digits)).empty()); //Check if false
-
       REQUIRE((*(test_ReallyLongInt->digits)).size() == 64);
-      REQUIRE_FALSE((*(test_ReallyLongInt->digits)).size()!=64); //Check if false
+      REQUIRE((*(test_ReallyLongInt->digits))[32] == 1);
 
       delete test_ReallyLongInt;
     }
 
-  SECTION("Max function")
+
+
+
+     SECTION("Copy Constructor")
     {
-      test_vector = new Vector(10, 5.5);      
-      REQUIRE(test_vector->max() == 5.5);
+     
+      ReallyLongInt other("-4294967296");
+      test_ReallyLongInt = new ReallyLongInt(other);
+
+      INFO("Using copy constructor") //Displayed if fails
+
+      REQUIRE(test_ReallyLongInt->size ==64); // Check if true
+      REQUIRE(test_ReallyLongInt->isNeg == true);
+      REQUIRE(!(*(test_ReallyLongInt->digits)).empty());
+      REQUIRE((*(test_ReallyLongInt->digits)).size() == 64);
+      REQUIRE((*(test_ReallyLongInt->digits))[32] == 1);
+
+      delete test_ReallyLongInt;
+      delete &other;
     }
+
+
+  SECTION("Equal test1")
+    {
+      test_ReallyLongInt = new ReallyLongInt();
+      ReallyLongInt testing; 
+
+      INFO("Testing equal") //Displayed if fails
+      REQUIRE(test_ReallyLongInt->equal(testing));
+    }
+
+    SECTION("Equal test2")
+    {
+      test_ReallyLongInt = new ReallyLongInt();
+      ReallyLongInt testing(2); 
+
+      INFO("Testing unequal") //Displayed if fails
+      REQUIRE(!test_ReallyLongInt->equal(testing));
+    }
+
+      SECTION("Greater test1")
+    {
+      test_ReallyLongInt = new ReallyLongInt(3);
+      ReallyLongInt testing(2); 
+
+      INFO("Testing greater") //Displayed if fails
+      REQUIRE(test_ReallyLongInt->greater(testing));
+    }
+
+    SECTION("Greater test2")
+    {
+      test_ReallyLongInt = new ReallyLongInt(2);
+      ReallyLongInt testing(2); 
+
+      INFO("Testing equal(greater)") //Displayed if fails
+      REQUIRE(!test_ReallyLongInt->greater(testing));
+    }
+
+    SECTION("Greater test3")
+    {
+      test_ReallyLongInt = new ReallyLongInt(2);
+      ReallyLongInt testing(3); 
+
+      INFO("Testing smaller(greater)") //Displayed if fails
+      REQUIRE(!test_ReallyLongInt->greater(testing));
+    }
+
+    SECTION("toString test")
+    {
+      test_ReallyLongInt = new ReallyLongInt(2);
+
+      INFO("Testing toString") //Displayed if fails
+      REQUIRE((test_ReallyLongInt->toString()).compare("2"));
+    }
+
+     SECTION("toBinary test")
+    {
+      test_ReallyLongInt = new ReallyLongInt(2);
+
+      INFO("Testing toBinaryString") //Displayed if fails
+      REQUIRE((test_ReallyLongInt->toStringBinary()).compare("0000000000000010"));
+    }
+
+
+
+
+
+
+
+
 }
 
-TEST_CASE("Testing Nothing")
-{
-  SECTION("Nothing One")
-    {
-      REQUIRE(true);
-    }
-
-
-}
 
