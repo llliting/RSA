4f6e26c1bd5fba31ce1f5b88d93d95ef523a80bd
llliting <hllliting@gmail.com>
2019-09-30 15:53:27 -0400

    ?

---
+++ encrypt.cpp
@@ -0,0 +1,20 @@
+#include "ReallyLongInt.hpp"
+#include "numberTheory.hpp"
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main(int argc, char *argv[]){
+    ofstream kfile(argv[1]);
+
+    string line;
+    getline(kfile, line);
+    
+        
+    myfile.close();
+  }
+
+
+
+}
