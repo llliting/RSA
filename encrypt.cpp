a2672ac62da2034590c8295e45dd76cf1718585b
llliting <lhuang1@fandm.edu>
2019-09-30 21:41:32 -0400

    encrypt yayy

--- encrypt.cpp
+++ encrypt.cpp
@@ -6,15 +6,26 @@
 using namespace std;
 
 int main(int argc, char *argv[]){
-    ofstream kfile(argv[1]);
+    ifstream kfile(argv[1]);
+    string e_str;
+    string n_str;
+    fin>>e_str;
+    fin>>n_str;
+    ReallyLongInt e(stoll(e_str));
+    ReallyLongInt n(stoll(n_str));
+    kfile.close();
 
-    string line;
-    getline(kfile, line);
-    
-        
-    myfile.close();
-  }
+    ifstream text(argv[2]);
+	  ofstream encrypt(argv[3]);
 
+    char c;
+	  while (text.get(c)){
+      unsigned long long x =(unsigned long long)c;
+      ReallyLongInt y = (x.exponent(e)) % n;
+      encrypt << y.toString();
+    }
 
+    text.close();
+    encrypt.close();
 
 }
